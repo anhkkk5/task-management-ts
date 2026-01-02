b1105f448192e611528d6d07cb504a90d2cb6adf
Nguyễn Tuấn Anh <baoawm2@gmail.com>
2026-01-02 10:10:29 +0700

    lấy chi tiết 1 việc

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ database.connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3002;
-
+// Lấy danh sách công việc
 app.get("/tasks", async (req: Request, res: Response) => {
   try {
     const tasks = await Task.find({
@@ -20,7 +20,25 @@ app.get("/tasks", async (req: Request, res: Response) => {
     res.status(500).json({ error: "Lỗi khi lấy danh sách công việc" });
   }
 });
+// Lấy chi tiết công việc
+app.get("/tasks/detail/:id", async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params.id;
+    const task = await Task.findOne({
+      deleted: false,
+      _id: id,
+    });
+    console.log(task);
+    if (!task) {
+      return res.status(404).json({ error: "Không tìm thấy công việc" });
+    }
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: "Lỗi khi lấy chi tiết công việc" });
+  }
+});
 
+// Lắng nghe cổng
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
