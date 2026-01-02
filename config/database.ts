3492698e9284a4503fee003e5bcd5b68d1207f87
Nguyễn Tuấn Anh <baoawm2@gmail.com>
2026-01-02 10:04:41 +0700

    lấy ra danh sách công việc

---
+++ config/database.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connect = async (): Promise<void> => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("Hello World1235");
+import express, { Express, Request, Response } from "express";
+import * as database from "./config/database";
+import dotenv from "dotenv";
+import Task from "./models/task.model";
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3002;
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  try {
+    const tasks = await Task.find({
+      deleted: false,
+    });
+    console.log(tasks);
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: "Lỗi khi lấy danh sách công việc" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
